dc08d7569a846e98c2ea6b8704d08d0a00d56934
Devjyoti Mohanty <68328634+Dev26112001@users.noreply.github.com>
2023-12-21 22:44:18 +0530

    Add files via upload

---
+++ Algo Trading System [Final Approach]/algorithmic-trading-main/config.py
@@ -0,0 +1,11 @@
+API_KEY = "CK5FYX8AGFRKQFM02ESF"
+SECRET_KEY = "t0baYkFPwyzEIeW7XwffTJY0FaCFTQ7niJZcWzE7"
+
+ALPACA_CONFIG = {
+# Put your own Alpaca key here:
+"API_KEY": "CK5FYX8AGFRKQFM02ESF",
+# Put your own Alpaca secret here:
+"API_SECRET": "t0baYkFPwyzEIeW7XwffTJY0FaCFTQ7niJZcWzE7",
+# If you want to go live, you must change this
+"ENDPOINT": "https://broker-api.sandbox.alpaca.markets",
+}
